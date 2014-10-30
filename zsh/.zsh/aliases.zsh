5630aa2a75fb6afc9ee779847042f0a8807af236
Tadej Novak <tadej@tano.si>
2014-10-30 22:09:23 +0100

    Fix Linux colors in zsh

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -2,4 +2,19 @@ alias st="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 
 if [[ $IS_MAC -eq 1 ]]; then
 	eval "$(hub alias -s)"
+
+	alias ll="ls -lh"
+	alias la="ls -A"
+	alias l="ls -C"
+fi
+
+if [[ $IS_LINUX -eq 1 ]]; then
+    alias ls="ls --color=auto"
+    alias grep="grep --color=auto"
+    alias fgrep="fgrep --color=auto"
+    alias egrep="egrep --color=auto"
+
+	alias ll="ls -lh"
+	alias la="ls -A"
+	alias l="ls -C"
 fi
