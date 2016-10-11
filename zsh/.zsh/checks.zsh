13b26e6bcc4caa1f2c67f0a151eab8233892f6a8
Tadej Novak <tadej@tano.si>
2016-10-11 13:12:37 +0200

    stderr logging cleanup

--- zsh/.zsh/checks.zsh
+++ zsh/.zsh/checks.zsh
@@ -8,18 +8,18 @@ if [[ $(uname) = 'Darwin' ]]; then
     IS_MAC=1
 fi
 
-if [[ -x `which brew` ]]; then
+if [[ -x `which brew 2> /dev/null` ]]; then
     HAS_BREW=1
 fi
 
-if [[ -x `which apt-get` ]]; then
+if [[ -x `which apt-get 2> /dev/null` ]]; then
     HAS_APT=1
 fi
 
-if [[ -x `which yum` ]]; then
+if [[ -x `which yum 2> /dev/null` ]]; then
     HAS_YUM=1
 fi
 
-if [[ -x `which virtualenv` ]]; then
+if [[ -x `which virtualenv 2> /dev/null` ]]; then
     HAS_VIRTUALENV=1
 fi
