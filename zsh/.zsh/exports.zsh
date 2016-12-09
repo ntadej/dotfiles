5f74ad44ade3ec449ae49d829dd519707a72a170
Tadej Novak <tadej@tano.si>
2016-12-09 13:46:19 +0100

    Make grep options macOS only

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -15,10 +15,6 @@ fi
 # Setup terminal, and turn on colors
 export TERM=xterm-256color
 
-# Enable color in grep
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='3;33'
-
 # This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions
 export ARCHFLAGS='-arch x86_64'
 
@@ -27,6 +23,10 @@ export PAGER='less'
 if [[ $IS_MAC -eq 1 ]]; then
 	export EDITOR='atom'
 	export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+	# Enable color in grep
+	export GREP_OPTIONS='--color=auto'
+	export GREP_COLOR='3;33'
 fi
 
 export NVM_DIR="$HOME/.nvm"
