dcd1a8665d767bd7e636381e97cbc45d79fd3bb4
Tadej Novak <tadej@tano.si>
2017-09-10 17:54:16 +0200

    Minor prompt changes

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,10 +1,10 @@
 autoload -U colors && colors
 
-PROMPT='${PR_BOLD_BLUE}%~%{$reset_color%} $ '
+PROMPT='${PR_BOLD_BLUE}%2~%{$reset_color%} $ '
 RPROMPT='${vcs_info_msg_0_}'
 
 if [[ $USER != 'tadej' ]]; then
-	PROMPT='${PR_BOLD_GREEN}%n%{$reset_color%}:${PR_BOLD_BLUE}%~%{$reset_color%} $ '
+	PROMPT='${PR_BOLD_GREEN}%n%{$reset_color%}:${PR_BOLD_BLUE}%2~%{$reset_color%} $ '
 fi
 
 if (($+SSH_CONNECTION)); then
