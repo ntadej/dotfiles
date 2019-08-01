76c0b9019bca00992fb769998229ede0c34d7742
Tadej Novak <tadej.novak@cern.ch>
2019-08-01 14:42:21 +0200

    History tweaks

--- zsh/.zsh/setopt.zsh
+++ zsh/.zsh/setopt.zsh
@@ -13,7 +13,7 @@ setopt extended_glob # treat #, ~, and ^ as part of patterns for filename genera
 
 # ===== History
 setopt append_history # Allow multiple terminal sessions to all append to one zsh command history
-setopt extended_history # save timestamp of command and duration
+# setopt extended_history # save timestamp of command and duration
 setopt inc_append_history # Add comamnds as they are typed, don't wait until shell exit
 setopt hist_expire_dups_first # when trimming history, lose oldest duplicates first
 setopt hist_ignore_dups # Do not write events to history that are duplicates of previous events
@@ -21,7 +21,7 @@ setopt hist_ignore_space # remove command line from history list when first char
 setopt hist_find_no_dups # When searching history don't display results already cycled through twice
 setopt hist_reduce_blanks # Remove extra blanks from each command line being added to history
 setopt hist_verify # don't execute, just expand history
-setopt share_history # imports new commands and appends typed commands to history
+# setopt share_history # imports new commands and appends typed commands to history
 
 # ===== Completion
 # setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word
