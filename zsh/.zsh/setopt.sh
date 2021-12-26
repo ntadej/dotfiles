3fdba62978fedded28a9eef98c26a84c3f8d13c1
Tadej Novak <tadej@tano.si>
2021-12-26 14:51:18 +0100

    Minor zsh tweaks

--- zsh/.zsh/setopt.sh
+++
@@ -1,5 +0,0 @@
-# ===== History
-setopt append_history # Allow multiple terminal sessions to all append to one zsh command history
-# setopt extended_history # save timestamp of command and duration
-setopt inc_append_history # Add comamnds as they are typed, don't wait until shell exit
-setopt hist_reduce_blanks # Remove extra blanks from each command line being added to history
