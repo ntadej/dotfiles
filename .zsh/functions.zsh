b0f78822b63667eaa967d57810ee87a417604f54
Tadej Novak <tadej.novak@cern.ch>
2022-11-14 14:23:04 +0100

    Update singularity helper functions

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -5,9 +5,33 @@ if (( $+commands[singularity] )) && [[ ! $IS_SINGULARITY -eq 1 ]]; then
     # Use singularity
     if [[ -n ${SINGULARITY_CENTOS7+x} ]]; then
       if [[ $IS_NAF -eq 1 ]]; then
-        singularity exec --contain --pwd "$(pwd)" --bind /afs:/afs --bind /cvmfs:/cvmfs --bind /data:/data --bind /nfs:/nfs --bind /pnfs:/pnfs --bind /var/tmp:/var/tmp --bind /tmp:/tmp "$SINGULARITY_CENTOS7" /bin/zsh -l
+        singularity exec --contain --pwd "$(pwd)" --bind /afs:/afs --bind /cvmfs:/cvmfs --bind /data:/data --bind /nfs:/nfs --bind /pnfs:/pnfs --bind /var/tmp:/var/tmp --bind /var/data:/var/data --bind /tmp:/tmp "$SINGULARITY_CENTOS7" /bin/zsh -l
       else
         "$SINGULARITY_CENTOS7" /bin/zsh -l
+      fi
+    fi
+  }
+
+  function centos7extra()
+  {
+    # Use singularity
+    if [[ -n ${SINGULARITY_CENTOS7_EXTRA+x} ]]; then
+      if [[ $IS_NAF -eq 1 ]]; then
+        singularity exec --contain --pwd "$(pwd)" --bind /afs:/afs --bind /cvmfs:/cvmfs --bind /data:/data --bind /nfs:/nfs --bind /pnfs:/pnfs --bind /var/data:/var/data --bind /var/tmp:/var/tmp --bind /tmp:/tmp "$SINGULARITY_CENTOS7_EXTRA" /bin/zsh -l
+      else
+        "$SINGULARITY_CENTOS7_EXTRA" /bin/zsh -l
+      fi
+    fi
+  }
+
+  function centos7arc()
+  {
+    # Use singularity
+    if [[ -n ${SINGULARITY_CENTOS7_ARC+x} ]]; then
+      if [[ $IS_NAF -eq 1 ]]; then
+        singularity exec --contain --pwd "$(pwd)" --bind /afs:/afs --bind /cvmfs:/cvmfs --bind /data:/data --bind /nfs:/nfs --bind /pnfs:/pnfs --bind /var/data:/var/data --bind /var/tmp:/var/tmp --bind /tmp:/tmp "$SINGULARITY_CENTOS7_ARC" /bin/zsh -l
+      else
+        "$SINGULARITY_CENTOS7_ARC" /bin/zsh -l
       fi
     fi
   }
@@ -17,8 +41,8 @@ fi
 function lxplus()
 {
   if [[ $OSTYPE == darwin* ]]; then
-		kswitch -p tadej@CERN.CH
-	fi
+    kswitch -p tadej@CERN.CH
+  fi
 
   if [[ "${1}" == "dev" ]]; then
     ssh "lxplus-dev" -o UserKnownHostsFile=~/.ssh/known_lxplus "${@:2}"
@@ -31,12 +55,12 @@ function f9()
 {
   if [[ $OSTYPE == darwin* ]]; then
     kswitch -p tadej@F9.IJS.SI
-	fi
+  fi
 
   if [[ -z ${1+x} ]]; then
-	  pc=19
+    pc=19
   else
-	  pc=$1
+    pc=$1
   fi
 
   ssh -Y "f9pc${pc}.ijs.si" -o UserKnownHostsFile=~/.ssh/known_f9 "${@:2}"
@@ -46,7 +70,7 @@ function naf()
 {
   if [[ $OSTYPE == darwin* ]]; then
     kswitch -p tadej@DESY.DE
-	fi
+  fi
 
   if [[ "${1}" == "dev" ]]; then
     ssh -Y "naf-dev" -o UserKnownHostsFile=~/.ssh/known_naf "${@:2}"
@@ -59,7 +83,7 @@ function naf()
 function setupATLAS()
 {
   if [[ -z ${ATLAS_LOCAL_ROOT_BASE+x} ]]; then
-	  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+    export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
   fi
 
   source "${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh" "${@}"
